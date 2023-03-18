5567e9982dca0127615f5e266e92fcd7de070039
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-03-18 14:10:22 -0400

    Continues functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate"){
+    return (`This Saturday, I want to ${fun}!`);
+}
+function mondayWork(activity = "go to the office"){
+return(`This Monday, I will ${activity}.`)
+}
+function wrapAdjective(string = "||"){
+    return function(value = "hard worker"){
+        return `You are ${string}${value}${string}!`
+    }
+    
+}
+        
+    
+
+
